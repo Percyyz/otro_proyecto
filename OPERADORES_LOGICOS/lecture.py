02da236bfe4ac4732d4fb0811bb11164066405ee
percyz30 <percynoeyarihuaman@gmail.com>
2024-05-06 08:37:43 -0500

    guargo el proyecto como lecture

---
+++ OPERADORES_LOGICOS/lecture.py
@@ -0,0 +1,25 @@
+
+##OPERADOR "AND"
+#Si tienes dos variables booleanas, x e y, puedes hacer algo como esto
+x = True
+y = False
+
+resultado = x and y
+print(resultado)  # Esto imprimirá False
+
+
+##OPERADOR "OR"
+#Usando el operador or, puedes combinar dos condiciones y obtener un resultado verdadero si 
+#al menos una de las condiciones es verdadera.
+x = True
+y = False
+
+resultado = x or y
+print(resultado)  # Esto imprimirá True
+
+##OPERADOR "NOT"
+#El operador not niega el valor de una expresión booleana.
+x = True
+
+resultado = not x
+print(resultado)  # Esto imprimirá False
